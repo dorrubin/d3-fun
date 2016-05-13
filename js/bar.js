19a2417409f0ea6916ca79832e910185a7d31b96
Dor Rubin <dor.rubin@gmail.com>
2016-05-13 12:50:16 -0400

    commented code

--- js/bar.js
+++ js/bar.js
@@ -3,30 +3,30 @@
 var margin = {top: 20, right: 20, bottom: 30, left: 40},
 		width = 960 - margin.left - margin.right,
 		height = 500 - margin.top - margin.bottom;
-
-var formatPercent = d3.format("");
-
+ 
+var numberFormat = d3.format("d");
+//rangeRoundBands to include padding
 var x = d3.scale.ordinal()
 		.rangeRoundBands([0, width], 0.1, 1);
 
 var y = d3.scale.linear()
 		.range([height, 0]);
-
+// create x axis
 var xAxis = d3.svg.axis()
 		.scale(x)
 		.orient("bottom");
-
+// create y axis
 var yAxis = d3.svg.axis()
 		.scale(y)
 		.orient("left")
-		.tickFormat(formatPercent);
-
+		.tickFormat(numberFormat);
+// create canvas
 var svg = d3.select("body").append("svg")
 		.attr("width", width + margin.left + margin.right)
 		.attr("height", height + margin.top + margin.bottom)
-	.append("g")
+		.append("g")
 		.attr("transform", "translate(" + margin.left + "," + margin.top + ")");
-
+// loading in both complaints and data poopulation but only using complaints at the moment
 d3.csv("data/mini-complaints.csv", function(csv_complaint) {
 	d3.csv("data/population.csv", function(csv_pop) {
 		var complaint_data = d3.nest()
@@ -40,9 +40,9 @@ d3.csv("data/mini-complaints.csv", function(csv_complaint) {
 		// var population_data = d3.nest()
 		// 	.key(function(d) {return d.State;})
 		// 	.entries(csv_pop);
-
+		//range of data coming in
 		x.domain(complaint_data.map(function(d) { return d.key; }));
-		y.domain([0, d3.max(complaint_data, function(d) { return d.values; })]);
+		y.domain([0, d3.max(complaint_data, function(d) { return d.values; })]); //start at 0 -> max count
 
 		svg.append("g")
 				.attr("class", "x axis")
@@ -52,7 +52,7 @@ d3.csv("data/mini-complaints.csv", function(csv_complaint) {
 		svg.append("g")
 				.attr("class", "y axis")
 				.call(yAxis)
-			.append("text")
+				.append("text")
 				.attr("transform", "rotate(-90)")
 				.attr("y", 6)
 				.attr("dy", ".71em")
@@ -61,23 +61,24 @@ d3.csv("data/mini-complaints.csv", function(csv_complaint) {
 
 		svg.selectAll(".bar")
 				.data(complaint_data)
-			.enter().append("rect")
-				.attr("class", "bar")
-				.on("mouseenter", function(d) {
-							d3.select(this)
-								.style("cursor", "pointer");
-					})
-				.on("click", function(d) {
-						console.log("click: " + d.key);
-						window.location.href = "./index.html?state=" + d.key;
-					})
-				.attr("x", function(d) { return x(d.key); })
-				.attr("width", x.rangeBand())
-				.attr("y", function(d) { return y(d.values); })
-				.attr("height", function(d) { return height - y(d.values); });
+				.enter()
+					.append("rect")
+					.attr("class", "bar")
+					.on("mouseenter", function(d) {
+								d3.select(this)
+									.style("cursor", "pointer");
+						})
+					.on("click", function(d) {
+							console.log("click: " + d.key);
+							window.location.href = "./index.html?state=" + d.key;
+						})
+					.attr("x", function(d) { return x(d.key); })
+					.attr("width", x.rangeBand())
+					.attr("y", function(d) { return y(d.values); })
+					.attr("height", function(d) { return height - y(d.values); });
 
 		d3.select("input").on("change", change);
-
+		// sorting function
 		function change() {
 			// Copy-on-write since tweens are evaluated after a delay.
 			var x0 = x.domain(complaint_data.sort(this.checked ?
@@ -88,17 +89,17 @@ d3.csv("data/mini-complaints.csv", function(csv_complaint) {
 
 			svg.selectAll(".bar")
 					.sort(function(a, b) { return x0(a.key) - x0(b.key); });
-
-			var transition = svg.transition().duration(750),
-					delay = function(d, i) { return i * 50; };
+			// declare both the transition and delay
+			var transition = svg.transition().duration(750);
+			var	delay = function(d, i) { return i * 50; };
 
 			transition.selectAll(".bar")
 					.delay(delay)
 					.attr("x", function(d) { return x0(d.key); });
-
+			//call(xAxis) calls 
 			transition.select(".x.axis")
 					.call(xAxis)
-				.selectAll("g")
+					.selectAll("g")
 					.delay(delay);
 		}  //end change
 	});
--- js/pie.js
+++ js/pie.js
@@ -6,37 +6,37 @@ var margin = {top: 80, right: 20, bottom: 80, left: 40},
 		width = 960 - margin.left - margin.right,
 		height = 600 - margin.top - margin.bottom,
 		radius = Math.min(width, height) / 2 - animationLength;
-
+// color pallette
 var color = d3.scale.category20();
-
+// svg arc shape generator
 var arc = d3.svg.arc()
 		.outerRadius(radius - animationLength)
 		.innerRadius(100);
-
+// final shape for the hover over shape
 var arcOver = d3.svg.arc()
 		.outerRadius(radius + animationLength)
 		.innerRadius(100);
-
+// where labels are placed
 var labelArc = d3.svg.arc()
-		.outerRadius(radius + 10)
+		.outerRadius(radius + animationLength)
 		.innerRadius(radius);
-
+// creates array of objects with start/end angles to pass shape generator
 var pie = d3.layout.pie()
 		.sort(null)
 		.value(function(d) { return d.values; });
-
+// svg canvas --- tried responsiveness but didnt quite work
 var svg = d3.select("#container").append("svg")
 		.attr("width", "100%")
 		.attr("height", height + margin.top + margin.bottom)
 		.append("g")
-		.attr("transform", "translate(" + width / 2 + "," + 380 +  ")");
-
+		.attr("transform", "translate(" + width / 2 + "," + 380 +  ")"); //hardcoded to allow the labels to fit
+//function to place labels based on their 
 var getAngle = function (d) {
 		return (180 / Math.PI * (d.startAngle + d.endAngle) / 2 - 90);
 };
 
-var pos = d3.svg.arc().innerRadius(radius + 2).outerRadius(radius + 2);
-
+var pos = d3.svg.arc().innerRadius(radius).outerRadius(radius); //hardcoded for the labels
+//load data and the invoke callback
 d3.csv("data/mini-complaints.csv", function(csv_data){
 	var total = 0;
 	var nested_data = d3.nest()
@@ -49,14 +49,14 @@ d3.csv("data/mini-complaints.csv", function(csv_data){
 		var g = svg.selectAll(".arc")
 			.data(pie(nested_data))
 			.enter().append("g")
-			.attr("class", "arc");
+			.attr("class", "arc"); //group to translate so that the labels will fit
 
-		g.append("path")
-				.attr("d", arc)
+		g.append("path") //generate arcs with event listeners
+				.attr("d", arc) //d property assigned by the shape generator
 				.style("fill", function(d) { return color(d.data.key); })
 				.on("click", function(d) {
 						console.log("click: " + d.data.key);
-						window.location.href = "./index.html?product=" + d.data.key;
+						window.location.href = "./index.html?product=" + d.data.key; //click off to table
 				})
 				.on("mouseenter", function(d) {
 						console.log(d.data.key);
@@ -65,12 +65,12 @@ d3.csv("data/mini-complaints.csv", function(csv_data){
 							.attr("stroke","white")
 							.transition()
 							.duration(1000)
-							.attr("d", arcOver);
+							.attr("d", arcOver); //transition to arcOver path
 				})
 				.on("mouseleave", function(d) {
 						console.log("exit");
 						d3.select(this).transition()
-							.attr("d", arc);
+							.attr("d", arc); //transition back to arc original path shape
 				});
 
 		g.append("text")
@@ -81,5 +81,5 @@ d3.csv("data/mini-complaints.csv", function(csv_data){
 				.classed("label", true)
 				.style("text-anchor", "start")
 				// .attr("visibility", "hidden")
-				.text(function(d){ return d.data.key});
+				.text(function(d){ return d.data.key}); //text-anchor so that svg text begins at point radius + 10
 });
